ba44c2419a1ae262b0ce35b309a1e7e45125ad3c
ksenia-khait <89581440+ksenia-khait@users.noreply.github.com>
2022-06-21 00:13:06 +0800

    app.use router removed

--- app.js
+++ app.js
@@ -3,16 +3,15 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 3000;
+const { PORT = 3000 } = process.env;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
-  family: 4
+  family: 4,
 });
 
-app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cardss'));
 
 app.listen(PORT, () => {
