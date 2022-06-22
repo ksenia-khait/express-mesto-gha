bbf7fdf029165f239100c7000fa264a00634907b
ksenia-khait <89581440+ksenia-khait@users.noreply.github.com>
2022-06-22 15:29:41 +0800

    test bages fixed

--- app.js
+++ app.js
@@ -8,10 +8,6 @@ const { PORT = 3000 } = process.env;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// mongoose.connect('mongodb://localhost:27017/mestodb', {
-//   family: 4,
-// });
-
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use((req, res, next) => {
