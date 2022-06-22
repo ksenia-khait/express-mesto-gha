0ba686d2341620adbf0a89aedfe26b9143480d55
ksenia-khait <89581440+ksenia-khait@users.noreply.github.com>
2022-06-22 16:34:49 +0800

    eslintchanges

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const { PORT = 3000 } = process.env;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use((req, res, next) => {
@@ -21,8 +20,9 @@ app.use((req, res, next) => {
 
 app.use('/', require('./routes/users'));
 app.use('/', require('./routes/cardss'));
+
 app.use('*', (req, res) => {
-res.status(404).send({ message: 'Страница не найдена' });
+  res.status(404).send({ message: 'Страница не найдена' });
 });
 
 app.listen(PORT, () => {
