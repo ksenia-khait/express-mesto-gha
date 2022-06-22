e6497b5aca81482638ec4a0f31af1ca824c5c869
ksenia-khait <89581440+ksenia-khait@users.noreply.github.com>
2022-06-22 16:31:28 +0800

    general route changes

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 app.use('/', require('./routes/users'));
 app.use('/', require('./routes/cardss'));
 app.use('*', (req, res) => {
-res.status(400).send({ message: 'Страница не найдена' })ж
+res.status(404).send({ message: 'Страница не найдена' });
 });
 
 app.listen(PORT, () => {
