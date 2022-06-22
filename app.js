1c660f18f2d0caf505166ba73c492f409f34204c
ksenia-khait <89581440+ksenia-khait@users.noreply.github.com>
2022-06-22 16:29:05 +0800

    general route

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const { PORT = 3000 } = process.env;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use((req, res, next) => {
@@ -20,6 +21,9 @@ app.use((req, res, next) => {
 
 app.use('/', require('./routes/users'));
 app.use('/', require('./routes/cardss'));
+app.use('*', (req, res) => {
+res.status(400).send({ message: 'Страница не найдена' })ж
+});
 
 app.listen(PORT, () => {
   console.log('started on', PORT);
