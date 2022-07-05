6f95e3fc2d109c76efaeba2993edb0406f45d819
ksenia-khait <89581440+ksenia-khait@users.noreply.github.com>
2022-07-06 00:37:15 +0800

    wrong route

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.post('/', validateCreateUser, createUser);
 app.use('/', isAuthorized, require('./routes/users'));
 app.use('/', isAuthorized, require('./routes/cardss'));
 
-app.use('/', (req, res, next) => {
+app.use('*', (req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
 });
 
