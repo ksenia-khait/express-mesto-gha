c6cfbeb13c3becd8743eacc7c7d87570530ca340
ksenia-khait <89581440+ksenia-khait@users.noreply.github.com>
2022-07-06 10:37:27 +0800

    users models

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+// const bcrypt = require('bcryptjs');
 const validator = require('validator');
-const NotAuthorizedError = require('../errors/unathorizedError');
+// const NotAuthorizedError = require('../errors/unathorizedError');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -42,20 +42,20 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email }).select('+password')
-    .then((user) => {
-      if (!user) {
-        return Promise.reject(new NotAuthorizedError('Неправильная почта или пароль!'));
-      }
-      return bcrypt.compare(password, user.password)
-        .then((matched) => {
-          if (!matched) {
-            return Promise.reject(new NotAuthorizedError(('Неправильная почта или пароль!')));
-          }
-          return user;
-        });
-    });
-};
+// userSchema.statics.findUserByCredentials = function (email, password) {
+//   return this.findOne({ email }).select('+password')
+//     .then((user) => {
+//       if (!user) {
+//         return Promise.reject(new NotAuthorizedError('Неправильная почта или пароль!'));
+//       }
+//       return bcrypt.compare(password, user.password)
+//         .then((matched) => {
+//           if (!matched) {
+//             return Promise.reject(new NotAuthorizedError(('Неправильная почта или пароль!')));
+//           }
+//           return user;
+//         });
+//     });
+// };
 
 module.exports = mongoose.model('user', userSchema);
