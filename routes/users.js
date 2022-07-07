1e0ffd6f2007f702d6187e2ffca5f6280dae0dcd
ksenia-khait <89581440+ksenia-khait@users.noreply.github.com>
2022-07-07 22:58:08 +0800

    eslint corrections

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
-const router = require('express').Router();
-const {celebrate, Joi} = require('celebrate');
+const router = require('express')
+  .Router();
+const {
+  celebrate,
+  Joi,
+} = require('celebrate');
 const {
   getUser,
   getUserById,
@@ -9,26 +13,38 @@ const {
 } = require('../controllers/users');
 
 const {
-  validateGetUserById,
-  validateGetAuthedUserInfo,
-  validateUpdateProfile,
   validateUpdateAvatar,
 } = require('../middlewares/validations');
 
 router.get('/users', getUser);
-router.get('/users/:userId', celebrate({
-    params: Joi.object().keys({
-      userId: Joi.string().length(24).hex().required(),
-    }),
+router.get(
+  '/users/:userId',
+  celebrate({
+    params: Joi.object()
+      .keys({
+        userId: Joi.string()
+          .length(24)
+          .hex()
+          .required(),
+      }),
   }),
   getUserById,
 );
 router.get('/users/me', getAuthedUserInfo);
-router.patch('/users/me', celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().required().min(2).max(30),
-      about: Joi.string().required().min(2).max(30),
-    }),
+router.patch(
+  '/users/me',
+  celebrate({
+    body: Joi.object()
+      .keys({
+        name: Joi.string()
+          .required()
+          .min(2)
+          .max(30),
+        about: Joi.string()
+          .required()
+          .min(2)
+          .max(30),
+      }),
   }),
   updateProfile,
 );
