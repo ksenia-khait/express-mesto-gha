14e2696a753049e3a3290127c4c1b9c455688b48
ksenia-khait <89581440+ksenia-khait@users.noreply.github.com>
2022-07-08 17:17:37 +0800

    Update isAuthorized.js

--- middlewares/isAuthorized.js
+++ middlewares/isAuthorized.js
@@ -1,26 +1,21 @@
+const jwt = require('jsonwebtoken');
 const { checkToken } = require('../helpers/jwtt');
 const User = require('../models/user');
 const UnauthorizedError = require('../errors/unathorizedError');
 
-const isAuthorized = (req, res, next) => {
-  const auth = req.headers.authorization;
-  if (!auth) {
-    throw new UnauthorizedError('Необходимо пройти авторизацию');
+module.exports.isAuthorized = (req, res, next) => {
+  const { auth } = req.headers;
+  if (!auth || !auth.startsWith('Bearer ')) {
+    return next(new UnauthorizedError('Необходимо пройти авторизацию'));
   }
   const token = auth.replace('Bearer ', '');
+  console.log(token);
+  let payload;
   try {
-    const payload = checkToken(token);
-    User.findOne({ email: payload.email })
-      .then((user) => {
-        if (!user) {
-          return next(new UnauthorizedError('Необходимо пройти аторизацию'));
-        }
-        req.user = { id: user._id };
-        return next();
-      });
+    payload = jwt.verify(token, 'very-secret-key');
   } catch (err) {
-    next(new UnauthorizedError('Необходимо пройти авторизацию'));
+    return next(new UnauthorizedError('Необходимо пройти авторизацию'));
   }
+  req.user = payload;
+  return next();
 };
-
-module.exports = { isAuthorized };
