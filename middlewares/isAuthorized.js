379f2d1da3a796772afae1c93e14d9e0b541353a
ksenia-khait <89581440+ksenia-khait@users.noreply.github.com>
2022-07-08 12:32:02 +0800

    auth

--- middlewares/isAuthorized.js
+++ middlewares/isAuthorized.js
@@ -1,26 +1,45 @@
+const jwt = require('jsonwebtoken');
 const { checkToken } = require('../helpers/jwtt');
 const User = require('../models/user');
 const UnauthorizedError = require('../errors/unathorizedError');
 
+// const isAuthorized = (req, res, next) => {
+//   const auth = req.headers.authorization;
+//   if (!auth) {
+//     throw new UnauthorizedError('Необходимо пройти аторизацию');
+//   }
+//   const token = auth.replace('Bearer ', '');
+//   try {
+//     const payload = checkToken(token);
+//     User.findOne({ email: payload.email })
+//       .then((user) => {
+//         if (!user) {
+//           return next(new UnauthorizedError('Необходимо пройти аторизацию'));
+//         }
+//         req.user = { id: user._id };
+//         next();
+//       });
+//   } catch (err) {
+//     next(new UnauthorizedError('Необходимо пройти аторизацию'));
+//   }
+// };
+//
+// module.exports = { isAuthorized };
+
 const isAuthorized = (req, res, next) => {
   const auth = req.headers.authorization;
-  if (!auth) {
-    return next(new UnauthorizedError('Необходимо пройти аторизацию'));
+  if (!auth || !auth.startsWith('Bearer ')) {
+    throw new UnauthorizedError('Необходимо пройти аторизацию');
   }
   const token = auth.replace('Bearer ', '');
+  let payload;
   try {
-    const payload = checkToken(token);
-    User.findOne({ email: payload.email })
-      .then((user) => {
-        if (!user) {
-          return next(new UnauthorizedError('Необходимо пройти аторизацию'));
-        }
-        req.user = { id: user._id };
-        next();
-      });
+    payload = jwt.verify(token, 'very-secret-key');
   } catch (err) {
     next(new UnauthorizedError('Необходимо пройти аторизацию'));
   }
+  req.user = payload;
+  return next();
 };
 
 module.exports = { isAuthorized };
